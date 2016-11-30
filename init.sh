5ba4272ef837bf71bcc132dfe95aadb3ef9acdc2
quincynicholson <quincy.nicholson@mail.ru>
2016-11-30 10:13:08 +0800

    1.9

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
